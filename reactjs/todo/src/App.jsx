3841c4ecd0ba3fdc1d2637e00b89c495674669d1
shivanshuofficials <shivanshuchauhanofficials@gmail.com>
2026-01-18 23:05:10 +0530

    feat: Add todolist styling and install various project dependencies.

---
+++ reactjs/todo/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todolist from './todolist'
+const App = () => {
+  return (
+    <>
+      <Todolist />
+    </>
+  )
+}
+
+export default App
---
+++ reactjs/todo/src/todolist.jsx
@@ -0,0 +1,10 @@
+import './todolist.css'
+const Todolist = () => {
+  return (
+    <>
+      <h4>Todo List</h4>
+    </>
+  )
+}
+
+export default Todolist
